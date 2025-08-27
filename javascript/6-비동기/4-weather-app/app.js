f395c127878dea7f70477e158660a98cea875f44
Junmo Yoo <jmyoow@gmail.com>
2025-08-27 15:55:37 +0800

    2025-08-27 수업완료

--- javascript/6-비동기/4-weather-app/app.js
+++ javascript/6-비동기/4-weather-app/app.js
@@ -1,1 +1,84 @@
-// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric
+import MiniAlert from './mini-alert.js';
+
+const API_KEY = 'https://api.openweathermap.org에서 발급받은 API 키를 넣으세요';
+
+// `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+const input = document.getElementById('city-input');
+const button = document.getElementById('search-btn');
+const cityName = document.getElementById('city-name');
+const weatherInfo = document.getElementById('weather-info');
+
+button.addEventListener('click', searchWeather);
+input.addEventListener('keydown', e => {
+  if (e.code === 'Enter') {
+    searchWeather();
+  }
+})
+
+async function searchWeather() {
+  const city = input.value.trim();
+  
+  if (!city) {
+    MiniAlert.fire({
+      // title: '앗!',
+      message: '도시 이름을 입력해 주세요.'
+    });
+    return;
+  }
+
+  try {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    if (!res.ok) throw new Error('도시를 찾을 수 없습니다!');
+
+    // 조회 성공
+    const data = await res.json();
+    const name = data.name;
+    const temp = data.main.temp;
+    const desc = data.weather[0].description;
+
+    cityName.textContent = name;
+    weatherInfo.textContent = `섭씨 ${temp}도 / ${desc}`;
+    input.value = '';
+
+    let weatherStr = '';
+    // console.log(desc.indexOf('cloud'));
+    if (desc.indexOf('cloud') >= 0) {
+      weatherStr = 'cloud';
+    } else if (desc.indexOf('rain') >= 0) {
+      weatherStr = 'rain';
+    } else if (desc.indexOf('snow') >= 0) {
+      weatherStr = 'snow';
+    } else if (desc.indexOf('clear') >= 0) {
+      weatherStr = 'clear';
+    } else {
+      weatherStr = '???';
+    }
+
+    setBackgroundEffect(weatherStr);
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function setBackgroundEffect(weather) {
+  switch (weather) {
+    case 'cloud':
+      document.body.style.background = 'gainsboro';
+      break;
+    case 'rain':
+      document.body.style.background = 'lightsteelblue';
+      break;
+    case 'snow':
+      document.body.style.background = 'aliceblue';
+      break;
+    case 'clear':
+      document.body.style.background = 'lightyellow';
+      break;
+    case '???':
+      document.body.style.background = 'fuchisia';
+      break;
+  }
+}
---
+++ javascript/6-비동기/4-weather-app/mini-alert.js
@@ -0,0 +1,100 @@
+export default class MiniAlert {
+  static fire({ title, message, closeBackdrop = true, onClose }) {
+    // close를 메서드로 따로 분리했을 경우, close 메서드에서 접근할 수 있도록 속성으로 설정
+    // this.closeBackdrop = closeBackdrop;
+    // this.onClose = onClose;
+
+    // <style>을 스크립트로 붙이기
+    // #mini-alert-style 요소가 없을 때만 스타일 붙이기
+    if (!document.getElementById('mini-alert-style')) {
+      const style = document.createElement('style');
+      style.id = 'mini-alert-style';
+      style.textContent = `
+        .mini-alert-backdrop {
+          display: flex; justify-content: center; align-items: center;
+          position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 1000;
+          background: rgba(0, 0, 0, 0.4);
+        }
+
+        .mini-alert {
+          min-width: 200px; max-width: 500px;
+          padding: 2rem;
+          border-radius: 8px;
+          background: white;
+          box-shadow: rgba(0, 0, 0, 0.2) 15px 15px 0;
+        }
+      `;
+      document.head.append(style);
+    }
+
+    // DOM 구조 만들기
+    // this.backdrop = document.createElement('div');
+    // const backdrop = this.backdrop;
+    const backdrop = document.createElement('div');
+    backdrop.classList.add('mini-alert-backdrop');
+
+    const modal = document.createElement('div');
+    modal.classList.add('mini-alert');
+    const display = title ? '' : 'style="display: none"';
+    modal.innerHTML = `
+      <div class="mini-alert-content">
+        <h2 ${display} class="mini-alert-title">${title}</h2>
+        <p class="mini-alert-message">${message}</p>
+        <button class="mini-alert-close-btn">확인</button>
+      </div>
+    `;
+
+    backdrop.append(modal);
+    document.body.append(backdrop);
+
+    // Event
+    const closeBtn = modal.querySelector('.mini-alert-close-btn');
+    closeBtn.addEventListener('click', () => {
+      // this.close();
+      close(closeBtn);
+      // backdrop.remove();
+      // if(onClose) onClose();
+    });
+
+    backdrop.addEventListener('click', () => {
+      // if (this.closeBackdrop) {
+      if (closeBackdrop) {
+        // this.close(backdrop);
+        close();
+      }
+      // if (closeBackdrop) {
+        // backdrop.remove();
+        // if(onClose) onClose();
+      // }
+    });
+
+    modal.addEventListener('click', e => {
+      e.stopPropagation();
+    });
+
+    function close(target) {
+      backdrop.remove();
+
+      if (
+        target === closeBtn
+        && typeof onClose === 'function'
+      ) {
+        onClose();
+      }
+    }
+
+  }
+
+  // close(target) {
+  //   this.backdrop.remove();
+
+  //   if (
+  //     target !== this.backdrop
+  //     && typeof this.onClose === 'function'
+  //   ) {
+  //     this.onClose();
+  //   }
+  // }
+}
+
+window.MiniAlert = MiniAlert; // 전역 공간에 공개
